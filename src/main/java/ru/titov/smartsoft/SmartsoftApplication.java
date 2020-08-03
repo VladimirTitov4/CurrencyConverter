a5f21961e26abfa62060a25b89e4866eef25df8c
Vladimir Titov <vtitov4@yandex.ru>
2020-08-03 18:44:33 +0400

    first commit

---
+++ src/main/java/ru/titov/smartsoft/SmartsoftApplication.java
@@ -0,0 +1,13 @@
+package ru.titov.smartsoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartsoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartsoftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/titov/smartsoft/SmartsoftApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.titov.smartsoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartsoftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
